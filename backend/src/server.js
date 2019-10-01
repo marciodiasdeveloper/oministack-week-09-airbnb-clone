05a8286cd99c3c8b9f8e64aeb8c01cc5a335c719
Developers <developers@MacBook-Pro-de-Developers.local>
2019-10-01 10:31:42 -0300

    Fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+// mongoose.connect('');
 
 app.use(express.json);
 app.use(routes);
