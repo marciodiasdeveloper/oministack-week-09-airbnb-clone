cdf4e76d7c1b95eb7d8e524d39d6bd869abd977e
Developers <developers@MacBook-Pro-de-Developers.local>
2019-10-04 23:11:54 -0300

    Fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+io.on('connection', socket => {
+    console.log('Usuário conectado', socket.id);
+});
 
 mongoose.connect('mongodb+srv://oministack:oministack@oministack9-95zvb.mongodb.net/oministack9?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -15,6 +23,6 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333, function() {
+server.listen(3333, function() {
     console.log('Node server running on port http://localhost:3333/');
 });
